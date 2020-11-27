c23a281ab3cd5a27c5b4e4e072c119e194fb6cba
Shaun Yap <me@shaunyap.ca>
2020-11-27 23:26:18 +0000

    Add db schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,37 @@
--- Drop and recreate Users table (Example)
-
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS comments CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAT(255) NOT NULL
 );
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  thumbnail_photo VARCHAR(255) NOT NULL,
+  date_created DATE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  tags_id INTEGER REFERENCES tags(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  is_liked BOOLEAN NOT NULL DEFAULT FALSE,
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
+  date_posted DATE,
+  comment_text TEXT
+)
+
+CREATE TABLE categories (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE
+)
