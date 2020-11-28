1a5bf40b2959e826a1e327afb6283ffdf260a18a
Monica Li <monicali@live.ca>
2020-11-28 21:12:49 +0000

    adds more user and post queries

--- db/queries/post_queries.sql
+++ db/queries/post_queries.sql
@@ -2,4 +2,19 @@
 SELECT id, user_id, thumbnail_photo, date_created, url, title, description, rating, is_liked
 FROM posts
 
--- make a new post
+-- retrieve all posts for a category
+SELECT id, user_id, thumbnail_photo, date_created, url, title, description, rating, is_liked
+FROM posts
+JOIN categories on category_id = categories.id
+WHERE categories.name = 'nature'
+
+-- retrive tags for a post
+SELECT id, category_id, post_id, name
+FROM tags
+JOIN posts on post_id = posts.id
+WHERE posts.id = 1
+
+-- retrieve all comments for a post
+SELECT id, user_id, post_id, date_posted, comment_text
+FROM comments
+WHERE post_id = '1'
