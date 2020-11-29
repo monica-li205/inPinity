c4f253147ed2932bc357eb695f73ac8a97f49d2f
Shaun Yap <me@shaunyap.ca>
2020-11-29 03:32:02 +0000

    Add post routes

--- routes/posts.js
+++ routes/posts.js
@@ -10,5 +10,24 @@ module.exports = (db, helpers) => {
     })
     .catch(err => err);
   });
+
+  // Get specific post by ID
+  router.get("/:id", (req, res) => {
+    helpers.getPostWithId(db, req.params.id)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => err);
+  })
+
+  // Edit specific post by ID
+  router.put("/:id", (req, res) => {
+    const queryParams = [req.body];
+    helpers.editPost(db, req.params.id, queryParams)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => err);
+  })
   return router;
 };
