e732139e2f40426115268e6c1f4cd3ce8e727e69
Monica Li <monicali@live.ca>
2020-11-29 05:48:30 +0000

    adds values to sample posts

--- db/seeds/02_posts.sql
+++ db/seeds/02_posts.sql
@@ -1,20 +1,30 @@
-INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description, is_liked)
-VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/planterina_thumbnail.png?raw=true', '2020-11-24',  'https://planterina.com/', 'Planterina', 'This is a cool website about plants', true),
+INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description)
+VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/planterina_thumbnail.png?raw=true', '2020-11-24',  'https://planterina.com/', 'Planterina', 'This is a cool website about plants'),
 
-VALUES (2, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+VALUES (2, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats'),
 
-VALUES (3, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+VALUES (3, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://www.adayinthekitchen.com/chinese-winter-solstice-soup/', 'Chinese Winter Solstice Soup', 'This is a cool recipe'),
 
-VALUES (4, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+VALUES (4, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'http://yarnandhooks.com/index.php/2017/12/19/chunky-sweater-free-crochet-pattern/', 'Chunky Sweater Pattern', 'This is a crochet patten'),
 
-VALUES (5, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+VALUES (5, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://www.fashionfurniture.com/blog/2019/06/05/design-home-around-theme/', 'Fashion Furiniture', 'This is a cool article on interior design'),
 
-VALUES (6, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+VALUES (6, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://blog.codewithgauri.tech/blog/how-to-build-creative-readme-for-your-github-profile/', 'How To Create a Creative Readme', 'This is a cool README'),
 
-VALUES (7, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+VALUES (7, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://mechanicalkeyboards.com/shop/', 'Mechanical Keyboards', 'This is a cool website selling mechanical keyboards'),
 
-VALUES (8, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+VALUES (8, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'http://www.matthiaslechner.com/zootopia.html', 'Zootopia concept Blog', 'This is a cool website about Zootopia'),
 
-VALUES (9, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+VALUES (9, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://syvertshop.threadless.com/designs/good-boy/mens/t-shirt?color=lemon', 'Cool T-shirt', 'This is a cool shirt'),
 
-VALUES (10, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+VALUES (10, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://gumroad.com/l/RiZKX', 'Free Procreate Brushes', 'This is a cool website with free Procreate brushes'),
+
+VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://patterncooler.com/', 'Free Pattern Generator', 'This is a cool background pattern generator'),
+
+VALUES (3, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://css-tricks.com/snippets/css/a-guide-to-flexbox/', 'Flexbox guide', 'This is a cool website about css'),
+
+VALUES (5, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://www.shopinsecta.com/collections/shoes', 'Insecta Shoes', 'This is a cool website selling shoes'),
+
+VALUES (7, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://www.youtube.com/pokerez', 'Pokemon Youtuber', 'This is a cool youtube channel about pokemon'),
+
+VALUES (9, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://sellfy.com/petermckinnon', 'Photography', 'This is a cool website about photography');
