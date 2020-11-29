58f765e21944d6a66becfff8b887a736a420ba09
Monica Li <monicali@live.ca>
2020-11-29 05:00:11 +0000

    adds thumbnails for sample database to doc

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,11 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, username, password, email)
-VALUES ('Alice Smith', 'Alice123', 'password', 'alice@gmail.com');
+VALUES ('Alice Smith', 'Alice123', 'password', 'Alice@gmail.com'),
+VALUES ('Monica Li', 'Monica123', 'password', 'Monica@gmail.com'),
+VALUES ('Isobel Gunn', 'Isobel123', 'password', 'Isobel@gmail.com'),
+VALUES ('Huzaifah Sharma', 'Huzaifah123', 'password', 'Huzaifah@gmail.com'),
+VALUES ('Taliyah Jacobson', 'Taliyah123', 'password', 'Taliyah@gmail.com'),
+VALUES ('Regan Vance', 'Regan123', 'password', 'Regan@gmail.com'),
+VALUES ('Frankie Decker', 'Frankie123', 'password', 'Frankie@gmail.com'),
+VALUES ('Anum Butler', 'Anum123', 'password', 'Anum@gmail.com'),
+VALUES ('Aneeka Tapia', 'Aneeka123', 'password', 'Aneeka@gmail.com');
--- db/seeds/02_posts.sql
+++ db/seeds/02_posts.sql
@@ -1,4 +1,20 @@
 INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description, is_liked)
-VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/planterina_thumbnail.png?raw=true', '2020-11-24',  'https://planterina.com/', 'Planterina', 'This is a cool website about plants', true);
-INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description, is_liked)
-VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true);
+VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/planterina_thumbnail.png?raw=true', '2020-11-24',  'https://planterina.com/', 'Planterina', 'This is a cool website about plants', true),
+
+VALUES (2, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+
+VALUES (3, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+
+VALUES (4, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+
+VALUES (5, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+
+VALUES (6, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+
+VALUES (7, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+
+VALUES (8, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+
+VALUES (9, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
+
+VALUES (10, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true),
