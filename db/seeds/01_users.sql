f7fd6875459d5302e95f251add71f57869522831
Monica Li <monicali@live.ca>
2020-11-29 22:02:24 +0000

    renames schema and seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,11 +1,12 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, username, password, email)
 VALUES ('Alice Smith', 'Alice123', 'password', 'Alice@gmail.com'),
-VALUES ('Monica Li', 'Monica123', 'password', 'Monica@gmail.com'),
-VALUES ('Isobel Gunn', 'Isobel123', 'password', 'Isobel@gmail.com'),
-VALUES ('Huzaifah Sharma', 'Huzaifah123', 'password', 'Huzaifah@gmail.com'),
-VALUES ('Taliyah Jacobson', 'Taliyah123', 'password', 'Taliyah@gmail.com'),
-VALUES ('Regan Vance', 'Regan123', 'password', 'Regan@gmail.com'),
-VALUES ('Frankie Decker', 'Frankie123', 'password', 'Frankie@gmail.com'),
-VALUES ('Anum Butler', 'Anum123', 'password', 'Anum@gmail.com'),
-VALUES ('Aneeka Tapia', 'Aneeka123', 'password', 'Aneeka@gmail.com');
+('Monica Li', 'Monica123', 'password', 'Monica@gmail.com'),
+('Isobel Gunn', 'Isobel123', 'password', 'Isobel@gmail.com'),
+('Huzaifah Sharma', 'Huzaifah123', 'password', 'Huzaifah@gmail.com'),
+('Taliyah Jacobson', 'Taliyah123', 'password', 'Taliyah@gmail.com'),
+('Regan Vance', 'Regan123', 'password', 'Regan@gmail.com'),
+('Frankie Decker', 'Frankie123', 'password', 'Frankie@gmail.com'),
+('Anum Butler', 'Anum123', 'password', 'Anum@gmail.com'),
+('Aneeka Tapia', 'Aneeka123', 'password', 'Aneeka@gmail.com'),
+('Leslie Nope', 'Leslie123', 'password', 'Leslie@gmail.com');
--- db/seeds/02_posts.sql
+++ db/seeds/02_posts.sql
@@ -2,30 +2,30 @@ INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, descripti
 -- id 1
 VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/01.png?raw=true', '2020-11-24',  'https://planterina.com/', 'Planterina', 'This is a cool website about plants', 'plants'),
 -- id 2
-VALUES (2, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/02.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', 'animals'),
+(2, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/02.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', 'animals'),
 -- id 3
-VALUES (3, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/03.png?raw=true', '2020-11-24',  'https://www.adayinthekitchen.com/chinese-winter-solstice-soup/', 'Chinese Winter Solstice Soup', 'This is a cool recipe', 'food'),
+(3, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/03.png?raw=true', '2020-11-24',  'https://www.adayinthekitchen.com/chinese-winter-solstice-soup/', 'Chinese Winter Solstice Soup', 'This is a cool recipe', 'food'),
 -- id 4
-VALUES (4, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/04.png?raw=true', '2020-11-24',  'http://yarnandhooks.com/index.php/2017/12/19/chunky-sweater-free-crochet-pattern/', 'Chunky Sweater Pattern', 'This is a crochet patten', 'diy'),
+(4, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/04.png?raw=true', '2020-11-24',  'http://yarnandhooks.com/index.php/2017/12/19/chunky-sweater-free-crochet-pattern/', 'Chunky Sweater Pattern', 'This is a crochet patten', 'diy'),
 -- id 5
-VALUES (5, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/05.png?raw=true', '2020-11-24',  'https://www.fashionfurniture.com/blog/2019/06/05/design-home-around-theme/', 'Fashion Furiniture', 'This is a cool article on interior design', 'interior design'),
+(5, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/05.png?raw=true', '2020-11-24',  'https://www.fashionfurniture.com/blog/2019/06/05/design-home-around-theme/', 'Fashion Furiniture', 'This is a cool article on interior design', 'interior design'),
 -- id 6
-VALUES (6, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/06.png?raw=true', '2020-11-24',  'https://blog.codewithgauri.tech/blog/how-to-build-creative-readme-for-your-github-profile/', 'How To Create a Creative Readme', 'This is a cool README', 'technology'),
+(6, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/06.png?raw=true', '2020-11-24',  'https://blog.codewithgauri.tech/blog/how-to-build-creative-readme-for-your-github-profile/', 'How To Create a Creative Readme', 'This is a cool README', 'technology'),
 -- id 7
-VALUES (7, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/07.png?raw=true', '2020-11-24',  'https://mechanicalkeyboards.com/shop/', 'Mechanical Keyboards', 'This is a cool website selling mechanical keyboards', 'technology'),
+(7, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/07.png?raw=true', '2020-11-24',  'https://mechanicalkeyboards.com/shop/', 'Mechanical Keyboards', 'This is a cool website selling mechanical keyboards', 'technology'),
 -- id 8
-VALUES (8, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/08.png?raw=true', '2020-11-24',  'http://www.matthiaslechner.com/zootopia.html', 'Zootopia concept Blog', 'This is a cool website about Zootopia', 'visual arts'),
+(8, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/08.png?raw=true', '2020-11-24',  'http://www.matthiaslechner.com/zootopia.html', 'Zootopia concept Blog', 'This is a cool website about Zootopia', 'visual arts'),
 -- id 9
-VALUES (9, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/09.png?raw=true', '2020-11-24',  'https://syvertshop.threadless.com/designs/good-boy/mens/t-shirt?color=lemon', 'Cool T-shirt', 'This is a cool shirt', 'fashion'),
+(9, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/09.png?raw=true', '2020-11-24',  'https://syvertshop.threadless.com/designs/good-boy/mens/t-shirt?color=lemon', 'Cool T-shirt', 'This is a cool shirt', 'fashion'),
 -- id 10
-VALUES (10, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/10.png?raw=true', '2020-11-24',  'https://gumroad.com/l/RiZKX', 'Free Procreate Brushes', 'This is a cool website with free Procreate brushes', 'visual arts'),
+(10, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/10.png?raw=true', '2020-11-24',  'https://gumroad.com/l/RiZKX', 'Free Procreate Brushes', 'This is a cool website with free Procreate brushes', 'visual arts'),
 -- id 11
-VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/11.png?raw=true', '2020-11-24',  'https://patterncooler.com/', 'Free Pattern Generator', 'This is a cool background pattern generator', 'visual arts'),
+(1, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/11.png?raw=true', '2020-11-24',  'https://patterncooler.com/', 'Free Pattern Generator', 'This is a cool background pattern generator', 'visual arts'),
 -- id 12
-VALUES (3, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/12.png?raw=true', '2020-11-24',  'https://css-tricks.com/snippets/css/a-guide-to-flexbox/', 'Flexbox guide', 'This is a cool website about css', 'technology'),
+(3, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/12.png?raw=true', '2020-11-24',  'https://css-tricks.com/snippets/css/a-guide-to-flexbox/', 'Flexbox guide', 'This is a cool website about css', 'technology'),
 -- id 13
-VALUES (5, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/13.png?raw=true', '2020-11-24',  'https://www.shopinsecta.com/collections/shoes', 'Insecta Shoes', 'This is a cool website selling shoes', 'fashion'),
+(5, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/13.png?raw=true', '2020-11-24',  'https://www.shopinsecta.com/collections/shoes', 'Insecta Shoes', 'This is a cool website selling shoes', 'fashion'),
 -- id 14
-VALUES (7, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://www.youtube.com/pokerez', 'Pokemon Youtuber', 'This is a cool youtube channel about pokemon', 'entertainment'),
+(7, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/14.png?raw=true', '2020-11-24',  'https://www.youtube.com/pokerez', 'Pokemon Youtuber', 'This is a cool youtube channel about pokemon', 'entertainment'),
 -- id 15
-VALUES (9, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://sellfy.com/petermckinnon', 'Photography', 'This is a cool website about photography', 'visual arts');
+(9, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/15.png?raw=true', '2020-11-24',  'https://sellfy.com/petermckinnon', 'Photography', 'This is a cool website about photography', 'visual arts');
