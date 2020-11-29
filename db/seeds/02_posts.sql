6e030a295e80b1f281bc900014d9f82440bc9297
Monica Li <monicali@live.ca>
2020-11-29 00:33:21 +0000

    adds thumbnail photo for cat blog. Changed values in sample post

--- db/seeds/02_posts.sql
+++ db/seeds/02_posts.sql
@@ -1,2 +1,4 @@
 INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description, rating, is_liked)
-VALUES (1, 'https://picture.com', '2020-11-24',  'https://title.com', 'This is a cool website', 'Example description here', 10, true);
+VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/planterina_thumbnail.png?raw=true', '2020-11-24',  'https://planterina.com/', 'Planterina', 'This is a cool website about plants', 5, true);
+INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description, rating, is_liked)
+VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/planterina_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', 5, true);
