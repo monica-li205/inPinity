774af60dce57eab1d9633fb7e7ed54d64c0be4fd
agxcd <agxcdcode@gmail.com>
2020-11-29 03:05:04 +0000

    flexbox not working

--- server.js
+++ server.js
@@ -1,48 +1,51 @@
 // load .env data into process.env
-require('dotenv').config();
+require("dotenv").config();
 
 // Web server config
-const PORT       = process.env.PORT || 8080;
-const ENV        = process.env.ENV || "development";
-const express    = require("express");
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
 const bodyParser = require("body-parser");
-const sass       = require("node-sass-middleware");
-const app        = express();
-const morgan     = require('morgan');
-const postHelpers = require('./db/db_helpers/posts.js');
-const userHelpers = require('./db/db_helpers/users.js');
-const cookieSession = require('cookie-session');
-
+const sass = require("node-sass-middleware");
+const app = express();
+const morgan = require("morgan");
+const postHelpers = require("./db/db_helpers/posts.js");
+const userHelpers = require("./db/db_helpers/users.js");
+const cookieSession = require("cookie-session");
 
 // PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
-db.connect()
-.catch(err => console.log(err));
+db.connect().catch((err) => console.log(err));
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
-}));
+app.use(
+  "/styles",
+  sass({
+    src: __dirname + "/styles",
+    dest: __dirname + "/public/styles",
+    debug: true,
+    outputStyle: "expanded",
+  })
+);
 app.use(express.static("public"));
-app.use(cookieSession({
-  name: 'session',
-  keys: ['secret'],
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["secret"],
 
-  // Cookie Options
-  maxAge: 24 * 60 * 60 * 1000 // 24 hours
-}))
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+);
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -56,7 +59,6 @@ app.use("/api/posts", postRoutes(db, postHelpers));
 app.use("/login", loginPageRouter());
 // Note: mount other resources here, using the same pattern above
 
-
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
@@ -64,25 +66,22 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-<<<<<<< HEAD
 app.get("/users", (req, res) => {
   res.render("users");
 });
 
-app.get("/create_post", (req, res) => {
+app.get("/cp", (req, res) => {
   res.render("create_post");
 });
 
-app.get("/create_board", (req, res) => {
+app.get("/cb", (req, res) => {
   res.render("create_board");
 });
 
-app.get("/user_post", (req, res) => {
-  res.render("user_post");
+app.get("/upo", (req, res) => {
+  res.render("user_posts");
 });
 
-=======
->>>>>>> 7b5597bad4634bba4601fc402b9c1d2b920328f3
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
