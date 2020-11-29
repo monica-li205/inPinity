77706260326f943b433abaeeec7ab1080efd557b
Monica Li <monicali@live.ca>
2020-11-29 23:24:44 +0000

    adds seeds to tags, comments and ratings tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,12 +1,22 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, username, password, email)
+-- id 1
 VALUES ('Alice Smith', 'Alice123', 'password', 'Alice@gmail.com'),
+-- id 2
 ('Monica Li', 'Monica123', 'password', 'Monica@gmail.com'),
+-- id 3
 ('Isobel Gunn', 'Isobel123', 'password', 'Isobel@gmail.com'),
+-- id 4
 ('Huzaifah Sharma', 'Huzaifah123', 'password', 'Huzaifah@gmail.com'),
+-- id 5
 ('Taliyah Jacobson', 'Taliyah123', 'password', 'Taliyah@gmail.com'),
+-- id 6
 ('Regan Vance', 'Regan123', 'password', 'Regan@gmail.com'),
+-- id 7
 ('Frankie Decker', 'Frankie123', 'password', 'Frankie@gmail.com'),
+-- id 8
 ('Anum Butler', 'Anum123', 'password', 'Anum@gmail.com'),
+-- id 8
 ('Aneeka Tapia', 'Aneeka123', 'password', 'Aneeka@gmail.com'),
+-- id 10
 ('Leslie Nope', 'Leslie123', 'password', 'Leslie@gmail.com');
