fea9e46d1974f6a72fdbdfc27fbb88f2fce75665
Shaun Yap <me@shaunyap.ca>
2020-11-30 00:07:45 +0000

    Resolve merge conflict

--- db/seeds/02_posts.sql
+++ db/seeds/02_posts.sql
@@ -1,6 +1,40 @@
+<<<<<<< routes
 INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description, is_liked)
 VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/planterina_thumbnail.png?raw=true', '2020-11-24',  'https://planterina.com/', 'Planterina', 'This is a cool website about plants', true);
 INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description, is_liked)
 VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true);
 INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description, is_liked)
 VALUES (5, 'https://github.com/psyphur/lhl-midterm/blob/master/docs/allaboutcats_thumbnail.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', true);
+=======
+INSERT INTO posts (user_id, thumbnail_photo, date_created, url, title, description, category)
+-- id 1
+VALUES (1, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/01.png?raw=true', '2020-11-24',  'https://planterina.com/', 'Planterina', 'This is a cool website about plants', 'plants'),
+-- id 2
+(2, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/02.png?raw=true', '2020-11-24',  'https://allaboutcats.com/', 'All About Cats', 'This is a cool website about cats', 'animals'),
+-- id 3
+(3, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/03.png?raw=true', '2020-11-24',  'https://www.adayinthekitchen.com/chinese-winter-solstice-soup/', 'Chinese Winter Solstice Soup', 'This is a cool recipe', 'food'),
+-- id 4
+(4, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/04.png?raw=true', '2020-11-24',  'http://yarnandhooks.com/index.php/2017/12/19/chunky-sweater-free-crochet-pattern/', 'Chunky Sweater Pattern', 'This is a crochet patten', 'diy'),
+-- id 5
+(5, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/05.png?raw=true', '2020-11-24',  'https://www.fashionfurniture.com/blog/2019/06/05/design-home-around-theme/', 'Fashion Furiniture', 'This is a cool article on interior design', 'interior design'),
+-- id 6
+(6, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/06.png?raw=true', '2020-11-24',  'https://blog.codewithgauri.tech/blog/how-to-build-creative-readme-for-your-github-profile/', 'How To Create a Creative Readme', 'This is a cool README', 'technology'),
+-- id 7
+(7, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/07.png?raw=true', '2020-11-24',  'https://mechanicalkeyboards.com/shop/', 'Mechanical Keyboards', 'This is a cool website selling mechanical keyboards', 'technology'),
+-- id 8
+(8, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/08.png?raw=true', '2020-11-24',  'http://www.matthiaslechner.com/zootopia.html', 'Zootopia concept Blog', 'This is a cool website about Zootopia', 'visual arts'),
+-- id 9
+(9, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/09.png?raw=true', '2020-11-24',  'https://syvertshop.threadless.com/designs/good-boy/mens/t-shirt?color=lemon', 'Cool T-shirt', 'This is a cool shirt', 'fashion'),
+-- id 10
+(10, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/10.png?raw=true', '2020-11-24',  'https://gumroad.com/l/RiZKX', 'Free Procreate Brushes', 'This is a cool website with free Procreate brushes', 'visual arts'),
+-- id 11
+(1, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/11.png?raw=true', '2020-11-24',  'https://patterncooler.com/', 'Free Pattern Generator', 'This is a cool background pattern generator', 'visual arts'),
+-- id 12
+(3, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/12.png?raw=true', '2020-11-24',  'https://css-tricks.com/snippets/css/a-guide-to-flexbox/', 'Flexbox guide', 'This is a cool website about css', 'technology'),
+-- id 13
+(5, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/13.png?raw=true', '2020-11-24',  'https://www.shopinsecta.com/collections/shoes', 'Insecta Shoes', 'This is a cool website selling shoes', 'fashion'),
+-- id 14
+(7, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/14.png?raw=true', '2020-11-24',  'https://www.youtube.com/pokerez', 'Pokemon Youtuber', 'This is a cool youtube channel about pokemon', 'entertainment'),
+-- id 15
+(9, 'https://github.com/psyphur/lhl-midterm/blob/feature_add_seeds/docs/15.png?raw=true', '2020-11-24',  'https://sellfy.com/petermckinnon', 'Photography', 'This is a cool website about photography', 'visual arts');
+>>>>>>> master
