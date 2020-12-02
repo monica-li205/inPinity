cd80c7675b87d5135a8da7109e60b0471ba551ba
Your Name <your@email.com>
2020-12-02 02:14:27 +0000

    working main page

--- db/db_helpers/users.js
+++ db/db_helpers/users.js
@@ -3,15 +3,19 @@
  * @param {String} email
  * @return {Promise<{}>}
  */
-const getUserWithEmail = function(db, email) {
-  return db.query(`
+const getUserWithEmail = function (db, email) {
+  return db
+    .query(
+      `
   SELECT id, name, username, password, email
   FROM users
   WHERE email = $1
-  `, [email])
-  .then(res => res.rows[0])
-  .catch(err => console.error('query error', err.stack));
-}
+  `,
+      [email]
+    )
+    .then((res) => res.rows[0])
+    .catch((err) => console.error("query error", err.stack));
+};
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -19,15 +23,19 @@ exports.getUserWithEmail = getUserWithEmail;
  * @param {string} id
  * @return {Promise<{}>}
  */
-const getUserWithId = function(db, id) {
-  return db.query(`
+const getUserWithId = function (db, id) {
+  return db
+    .query(
+      `
   SELECT *
   FROM users
   WHERE id = $1
-  `, [id])
-  .then(res => res.rows[0])
-  .catch(err => console.error('query error', err.stack));
-}
+  `,
+      [id]
+    )
+    .then((res) => res.rows[0])
+    .catch((err) => console.error("query error", err.stack));
+};
 exports.getUserWithId = getUserWithId;
 
 /**
@@ -35,15 +43,19 @@ exports.getUserWithId = getUserWithId;
  * @param {string} username
  * @return {Promise<{}>}
  */
-const getUserWithUsername = function(db, username) {
-  return db.query(`
+const getUserWithUsername = function (db, username) {
+  return db
+    .query(
+      `
   SELECT *
   FROM users
   WHERE username = $1
-  `, [username])
-  .then(res => res.rows[0])
-  .catch(err => console.error('query error', err.stack));
-}
+  `,
+      [username]
+    )
+    .then((res) => res.rows[0])
+    .catch((err) => console.error("query error", err.stack));
+};
 exports.getUserWithUsername = getUserWithUsername;
 
 /**
@@ -51,18 +63,22 @@ exports.getUserWithUsername = getUserWithUsername;
  * @param {{name: string, user.username, password: string, email: string}} user
  * @return {Promise<{}>}
  */
-const addUser =  function(db, user) {
-  return db.query(`
+const addUser = function (db, user) {
+  return db
+    .query(
+      `
   INSERT INTO users (name, username, password, email)
   VALUES ($1, $2, $3, $4)
   RETURNING *;
-  `, [user.name, user.username, user.password, user.email])
-  .then(res => res.rows)
-  .catch(err => err);
-}
+  `,
+      [user.name, user.username, user.password, user.email]
+    )
+    .then((res) => res.rows)
+    .catch((err) => err);
+};
 exports.addUser = addUser;
 
-const editUser = function(db, user, params) {
+const editUser = function (db, user, params) {
   const queryParams = [];
 
   let queryString = `UPDATE users SET`;
@@ -84,10 +100,11 @@ const editUser = function(db, user, params) {
   queryParams.push(user);
   queryString += ` WHERE id = $${queryParams.length}`;
 
-  return db.query(queryString,queryParams)
-  .then(res => res.rows)
-  .catch(err => err);
-}
+  return db
+    .query(queryString, queryParams)
+    .then((res) => res.rows)
+    .catch((err) => err);
+};
 exports.editUser = editUser;
 
 const totalPostsByUser = (db, id) => {
@@ -95,9 +112,10 @@ const totalPostsByUser = (db, id) => {
   SELECT count(posts.id)
   FROM posts
   WHERE user_id = $1;
-  `
-  return db.query(queryString, [id])
-  .then(data => data.rows[0])
-  .catch(err => err);
-}
+  `;
+  return db
+    .query(queryString, [id])
+    .then((data) => data.rows[0])
+    .catch((err) => err);
+};
 exports.totalPostsByUser = totalPostsByUser;
--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,63 @@
-const express = require('express');
+/* eslint-disable camelcase */
+const { response } = require("express");
+const express = require("express");
 const router = express.Router();
 
-module.exports = (db, helpers) => {
+
+module.exports = (db, userHelpers, postHelpers) => {
+  let templateVars;
   router.get("/", (req, res) => {
-    const templateVars = {
+    const offset = Number(Object.values(req.query));
+
+    templateVars = {
       user: undefined,
-      error: undefined
-    }
-    if (req.session.user_id) {
-      res.redirect("/main");
-    } else {
-      res.render("index", templateVars);
-    }
-  })
+      error: undefined,
+      posts: undefined,
+    };
+    postHelpers
+      .getAllPosts(db, offset)
+      .then((posts) => {
+        if (req.session.user_id) {
+          res.redirect("/main");
+        } else {
+          templateVars = {
+            user: undefined,
+            posts: posts,
+            error: undefined,
+          };
+          console.log(posts);
+          res.render("index", templateVars);
+        }
+      })
+      .catch((err) => err);
+  });
 
   router.get("/main", (req, res) => {
-    let templateVars = {};
-    helpers.getUserWithId(db, req.session.user_id)
-    .then(data => {
-      helpers.totalPostsByUser(db, data.id)
-      .then(result => {
+    const userSession = req.session.user_id;
+    const offset = Number(Object.values(req.query));
+
+    // can you send me the hangout link?
+    const getUserRecord = userHelpers.getUserWithId(db, userSession);
+    const getUserPostsCount = userHelpers.totalPostsByUser(db, userSession);
+    const getAllPosts = postHelpers.getAllPosts(db, offset);
+
+    Promise.all([getUserRecord, getUserPostsCount, getAllPosts])
+      .then(data => {
         templateVars = {
-          user: data,
-          count: result.count
-        } 
+          user: data[0],
+          count: data[1].count,
+          posts: data[2]
+        };
         res.render("main", templateVars);
       })
-      
-    })
-    .catch(err => err);
-  })
+      .catch(err => err);
+  });
 
   router.get("/cp", (req, res) => {
     let templateVars = {
-      user: helpers.getUserWithId(db, req.session.user_id)
+      user: userHelpers.getUserWithId(db, req.session.user_id),
     };
-    
+
     if (!req.session.user_id) {
       templateVars = { user: undefined };
     }
@@ -45,46 +67,47 @@ module.exports = (db, helpers) => {
 
   router.get("/login", (req, res) => {
     let templateVars = {
-      user: helpers.getUserWithId(db, req.session.user_id)
+      user: userHelpers.getUserWithId(db, req.session.user_id),
     };
-    
+
     if (!req.session.user_id) {
       templateVars = { user: undefined };
     }
     res.render("login", templateVars);
-  })
-
+  });
+  // login route
   router.post("/", (req, res) => {
     const user = req.body;
     const email = user.email;
 
-    helpers.getUserWithEmail(db, email)
-    .then(data => {
-      const userRecord = data;
-      
-      if (!userRecord || userRecord.password !== user.password) {
-        // res.status(400).send("Invalid login");
-        // res.status(401).send("Unauthorized");
-        const templateVars = {
-          user: undefined,
-          error: "Invalid login",
+    userHelpers
+      .getUserWithEmail(db, email)
+      .then((data) => {
+        const userRecord = data;
+
+        if (!userRecord || userRecord.password !== user.password) {
+          // res.status(400).send("Invalid login");
+          // res.status(401).send("Unauthorized");
+          const templateVars = {
+            user: undefined,
+            error: "Invalid login",
+          };
+          res.status(401).render("index", templateVars);
+          return;
         }
-        res.status(401).render("index", templateVars);
-        return;
-      }
-      
-      userRecord.password = undefined;
-      req.session.user_id = userRecord.id;
-      // res.render("main", templateVars);
-      res.redirect("/main");
-    })
-    .catch(err => err);
+        // fetch user -> need to find out the function
+        userRecord.password = undefined;
+        req.session.user_id = userRecord.id;
+        console.log("userRecord", userRecord);
+        res.redirect("/main");
+      })
+      .catch((err) => err);
   });
 
   router.post("/logout", (req, res) => {
     req.session = null;
     res.redirect("/");
-  })
-  
+  });
+
   return router;
-}
+};
