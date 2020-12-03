c82e97745f5ae75a8e3f34ddf757513debdaa5b6
agxcd <agxcdcode@gmail.com>
2020-12-03 18:35:03 +0000

    footer fixed

--- public/scripts/alert.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(e) {
-  // const $delete = $("#alert");
-  // $("#generate-image").click(function(e) {
-  //   console.log("generated")
-  //   $delete.attr("src", $("#image-url").val());
-  //   console.log($delete);
-  // })
-})
