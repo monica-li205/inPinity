060765dd48943f4752202d404952683c17b54e86
Shaun Yap <me@shaunyap.ca>
2020-12-04 07:32:02 +0000

    fix infinite scroll

--- public/scripts/index-posts.js
+++ public/scripts/index-posts.js
@@ -1,22 +1,29 @@
 let offset = 0;
 
 $(document).ready(function (e) {
-  console.log("got here");
+  let loadMoreDelay = false;
+
   $(window).scroll(function (e) {
-    if ($(window).scrollTop() + $(window).height() === $(document).height()) {
-      console.log("end");
-      loadMore();
+    if ($(window).scrollTop() + 1 >= $("#main-container").height() - $(window).height()) {
+      if (loadMoreDelay === false) {
+        loadMoreDelay = true;
+        loadMore()
+        setTimeout(function () {
+          loadMoreDelay = false;
+        }, 3000);
+      }
     }
-  });
-});
+  })
+})
 
 const loadMore = function () {
   offset += 20;
   $.ajax({
-    method: "GET",
-    url: `/?offset=${offset}`,
-  }).then((data) => {
-    const $data = $(data).find(".post-body");
-    $("#post-area").append($data);
-  });
-};
+      method: "GET",
+      url: `/?offset=${offset}`
+    })
+    .then(data => {
+      const $data = $(data).find(".post-body");
+      $("#post-area").append($data);
+    })
+}
--- public/scripts/main-posts.js
+++ public/scripts/main-posts.js
@@ -1,40 +1,29 @@
 let offset = 0;
 
 $(document).ready(function (e) {
-  window.onscroll = function(ev) {
-    if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
-      // you're at the bottom of the page
-      loadMore();
-      console.log("Bottom of page");
+  let loadMoreDelay = false;
+
+  $(window).scroll(function (e) {
+    if ($(window).scrollTop() + 1 >= $("#container").height() - $(window).height()) {
+      if (loadMoreDelay === false) {
+        loadMoreDelay = true;
+        loadMore()
+        setTimeout(function () {
+          loadMoreDelay = false;
+        }, 3000);
+      }
     }
-};
-  // $(window).scroll(function (e) {
-  //   if ($(window).scrollTop() + $(window).height() === $(document.body).height()) {
-  //     console.log("end");
-  //     loadMore();
-  //   }
-  // });
-});
+  })
+})
 
 const loadMore = function () {
   offset += 20;
   $.ajax({
-    method: "GET",
-    url: `/main?offset=${offset}`,
-  }).then((data) => {
-    const $data = $(data).find(".post-body");
-    $("#post-area").append($data);
-  });
-};
-
-// const loadMorePopular = function () {
-//   offset += 5;
-//   $.ajax({
-//     method: "GET",
-//     url: `/main?offset=${offset}`,
-//   }).then((data) => {
-//     const $data = $(data).find(".fav_box > article");
-//     console.log($data);
-//     $(".fav_box").append($data);
-//   });
-// };
+      method: "GET",
+      url: `/main?offset=${offset}`
+    })
+    .then(data => {
+      const $data = $(data).find(".post-body");
+      $("#post-area").append($data);
+    })
+}
