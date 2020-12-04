35e982ba05998a76ab1e11e14ab0bc0187a88258
Monica Li <monicali@live.ca>
2020-12-04 09:14:49 +0000

    fixes bug with likePost. posts can now be liked and unliked

--- routes/likes.js
+++ routes/likes.js
@@ -3,29 +3,45 @@ const router = express.Router();
 const postHelper = require("../db/db_helpers/posts.js");
 
 module.exports = (db, helpers) => {
-  router.post("/:id", (req, res) => {
+  router.post("/", (req, res) => {
     const user_id = req.session.user_id;
-    const post_id = req.params.id;
+    const post_id = req.body.post_id;
+    postHelper.likePost(db, user_id, post_id);
+    console.log(user_id, post_id);
+    res.send('hello');
 
-    postHelper
-      .likePost(db, user_id, post_id)
-      .then((data) => {
-        console.log("likePOST", data);
-        // res.redirect("/main");
-      })
-      .catch((err) => err);
-  });
-
-  router.post("/:id", (req, res) => {
-    const user_id = req.session.user_id;
-    const post_id = req.params.id;
-    postHelper
-      .dislikePost(db, user_id, post_id)
-      .then((data) => {
-        console.log("dislikePOST", data);
-        // res.redirect("/main");
-      })
-      .catch((err) => err);
-  });
+  })
   return router;
-};
+}
+
+// const express = require("express");
+// const router = express.Router();
+// const postHelper = require("../db/db_helpers/posts.js");
+
+// module.exports = (db, helpers) => {
+//   router.post("/:id", (req, res) => {
+//     const user_id = req.session.user_id;
+//     const post_id = req.params.id;
+
+//     postHelper
+//       .likePost(db, user_id, post_id)
+//       .then((data) => {
+//         console.log("likePOST", data);
+//         // res.redirect("/main");
+//       })
+//       .catch((err) => err);
+//   });
+
+//   router.post("/:id", (req, res) => {
+//     const user_id = req.session.user_id;
+//     const post_id = req.params.id;
+//     postHelper
+//       .dislikePost(db, user_id, post_id)
+//       .then((data) => {
+//         console.log("dislikePOST", data);
+//         // res.redirect("/main");
+//       })
+//       .catch((err) => err);
+//   });
+//   return router;
+// };
